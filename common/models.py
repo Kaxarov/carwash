11037780a09a4b72427c55f0061e6cb093cc3bb7
kaxarov <kaxarov2003@gmail.com>
2022-02-16 10:38:32 +0500

    added model: Funchtions, car washing additional functions

--- common/models.py
+++ common/models.py
@@ -20,8 +20,21 @@ class CarOption(BaseModel):
     title = models.CharField(_("Nomi"), max_length=128)
     type = models.CharField(_("Turi"), choices=CarOptions.choices, max_length=128)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = _("Mashina funksiyasi")
         verbose_name_plural = _("Mashina funksiyalari")
 
 
+class Functions(BaseModel):
+    title = models.CharField(_("Nomi"), max_length=128)
+    price = models.DecimalField(_("Narxi"), decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Qo'shimcha hizmat")
+        verbose_name_plural = _("Qo'shimcha hizmatlar")
